2388d8f12ba8f69bccada9adb41a2906ff15ce71
Yafté M <redyafte@gmail.com>
2020-03-18 19:41:07 -0600

    Start project

---
+++ notification_system/settings_example.py
@@ -0,0 +1,25 @@
+import os
+
+SECRET_KEY = ''
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = []
+
+MIDDLEWARE = []
+
+ROOT_URLCONF = 'notification_system.urls'
+
+WSGI_APPLICATION = 'notification_system.wsgi.application'
+
+LANGUAGE_CODE = 'es'
+
+TIME_ZONE = 'America/Mexico_City'
+
+USE_I18N = False
+
+USE_L10N = False
+
+USE_TZ = True
